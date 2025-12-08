50f9d677b529f263dc1b53cdc587897c57bee272
07simi <simhudy@gmail.com>
2025-12-08 10:05:18 +0100

    Trieda Nastroj

---
+++ Nastroj.java
@@ -0,0 +1,36 @@
+public class Nastroj {
+
+    private String druh;
+    private double cena;
+    private String zvuk;
+    private int pocet;
+
+    public Nastroj(String druh, double cena, String zvuk, int pocet) {
+        this.druh = druh;
+        this.cena = cena;
+        this.zvuk = zvuk;
+        this.pocet = pocet;
+    }
+
+    public String getDruh() { return druh; }
+    public void setDruh(String druh) { this.druh = druh; }
+
+    public double getCena() { return cena; }
+    public void setCena(double cena) { this.cena = cena; }
+
+    public String getZvuk() { return zvuk; }
+    public void setZvuk(String zvuk) { this.zvuk = zvuk; }
+
+    public int getPocet() { return pocet; }
+    public void setPocet(int pocet) { this.pocet = pocet; }
+
+    @Override
+    public String toString() {
+        return "Nastroj{" +
+                "druh='" + druh + '\'' +
+                ", cena=" + cena +
+                ", zvuk='" + zvuk + '\'' +
+                ", pocet=" + pocet +
+                '}';
+    }
+}
