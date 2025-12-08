5c55de9c35c9d2757bbf85efb5696c150d243a14
07simi <simhudy@gmail.com>
2025-12-08 10:02:39 +0100

    Trieda Hrac

---
+++ Hrac.java
@@ -0,0 +1,37 @@
+public class Hrac {
+
+    private String meno;
+    private String priezvisko;
+    private String nastroj;
+    private double hodinovaSadzba;
+
+    public Hrac(String meno, String priezvisko, String nastroj, double hodinovaSadzba) {
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.nastroj = nastroj;
+        this.hodinovaSadzba = hodinovaSadzba;
+    }
+
+    public String getMeno() { return meno; }
+    public void setMeno(String meno) { this.meno = meno; }
+
+    public String getPriezvisko() { return priezvisko; }
+    public void setPriezvisko(String priezvisko) { this.priezvisko = priezvisko; }
+
+    public String getNastroj() { return nastroj; }
+    public void setNastroj(String nastroj) { this.nastroj = nastroj; }
+
+    public double getHodinovaSadzba() { return hodinovaSadzba; }
+    public void setHodinovaSadzba(double hodinovaSadzba) { this.hodinovaSadzba = hodinovaSadzba; }
+
+    @Override
+    public String toString() {
+        return "Hrac{" +
+                "meno='" + meno + '\'' +
+                ", priezvisko='" + priezvisko + '\'' +
+                ", nastroj='" + nastroj + '\'' +
+                ", hodinovaSadzba=" + hodinovaSadzba +
+                '}';
+    }
+}
+
